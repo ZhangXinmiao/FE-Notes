c9100649c8699eaa3b95eedac0a754c5f52a7199
张鑫淼 <1243235082@qq.com>
2021-03-09 01:24:42 +0800

    添加bind的第二个版本

--- 手写代码/bind.js
+++ 手写代码/bind.js
@@ -23,3 +23,35 @@ var test = person.getName;
 // console.log(test());
 // console.log(test.bind(person)());
 console.log(test.Mybind(person, 'haha', '!')())
+
+
+// 原有的版本有一个遗漏的点：bind的第参数作为this，其余的参数将作为bind创建的新函数的参数
+Function.prototype.myBind = function () {
+    var func = this;
+    var args = Array.prototype.slice.call(arguments, 0);
+    var newContext = args[0];
+    var initArgs = args.slice(1);
+    return function () {
+        func.apply(newContext, initArgs.concat(Array.prototype.slice.call(arguments, 0)));
+    };
+};
+
+// case
+this.x = 9;    
+var module = {
+  x: 81,
+  getX: function(a, b, c) {
+    console.log(a, b, c);
+    return this.x; 
+  }
+};
+
+console.log(module.getX()); // ?
+
+var retrieveX = module.getX;
+console.log(retrieveX()); // ?
+
+var boundGetX = retrieveX.bind(module, 1, 2);
+console.log(boundGetX(3)); // ?
+var bounfGetX1 = retrieveX.myBind(module, 1, 2);
+console.log(bounfGetX1(3));
